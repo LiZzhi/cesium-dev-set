d0414047e76581427dde904332ce12c4968586b9
XingTao <xingt@geovis.com.cn>
2023-08-24 09:58:53 +0800

    comm-button 添加注释

--- src/components/common/CommButton.vue
+++ src/components/common/CommButton.vue
@@ -1,3 +1,13 @@
+<!--
+ * @Author: XingTao xingt@geovis.com.cn
+ * @Date: 2023-08-23 13:43:50
+ * @LastEditors: XingTao xingt@geovis.com.cn
+ * @LastEditTime: 2023-08-24 09:29:33
+ * @FilePath: \cesium-secdev-set\src\components\common\CommButton.vue
+ * @Description: 自定义公共组件(按钮)
+ *	props属性:
+ *		1.class { string } default:"" class名，用来修改样式
+-->
 <template>
     <button class="comm-button" :class="props.class">
         <span class="comm-button-context">
