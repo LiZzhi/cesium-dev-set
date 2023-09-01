6134bf45db49a909e4fbd70da59def94a3c26dbf
XingTao <xingt@geovis.com.cn>
2023-09-01 10:21:32 +0800

    添加mapv

--- src/thirdParty/script.ts
+++ src/thirdParty/script.ts
@@ -1,6 +1,10 @@
 import * as Cesium from "cesium";
+import * as mapv from "@/thirdParty/mapv/mapv.js";
+import MapVLayer from "@/thirdParty/mapv/MapVLayer.js";
 
 window.Cesium = Cesium;
+window.mapv = mapv;
+window.MapVLayer = MapVLayer;
 // token
 const cesiumToken =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmOTNiYjkwZi1iMzRlLTRjZWQtYWQxMy00MDVmMjk4YTc0YmMiLCJpZCI6MzY3MDksImlhdCI6MTY1NTE3OTc1N30.fv4nNIkCEEy3VqlaekWVcE1btEcge5_zCl_36AtusT0";
