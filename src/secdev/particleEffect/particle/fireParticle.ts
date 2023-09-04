e5b5dcf557a1eb3da9659a3d8fa43fce60d4b9f4
XingTao <xingt@geovis.com.cn>
2023-09-04 14:31:16 +0800

    修改存取器

--- src/secdev/particleEffect/particle/fireParticle.ts
+++ src/secdev/particleEffect/particle/fireParticle.ts
@@ -1,22 +1,25 @@
 import { Viewer, Cartesian3} from "cesium";
 import particleSystemBase from "./particleSystemBase";
 
-export type fireStyleType = {
-    image?: string,
+export type fireUpdateType = {
     startScale?: number,
     endScale?: number,
     minimumParticleLife?: number,
     maximumParticleLife?: number,
     minimumSpeed?: number,
     maximumSpeed?: number,
-    particleSize?: number;
     emissionRate?: number,
+}
+
+export type fireInitialType = fireUpdateType & {
+    image?: string,
+    particleSize?: number;
     pitch?: number;
     heading?: number;
 }
 
 export default class fireParticle extends particleSystemBase {
-    #style: fireStyleType;
+    #style: fireInitialType;
     /**
      * @description: 火焰粒子效果
      * @param {Viewer} viewer viewer
@@ -27,7 +30,7 @@ export default class fireParticle extends particleSystemBase {
     constructor(
         viewer: Viewer,
         position: Cartesian3,
-        style: fireStyleType = {}
+        style: fireInitialType = {}
     ) {
         super(viewer, position);
         this.#style = Object.assign(this.defaultStyle, style);
@@ -56,7 +59,7 @@ export default class fireParticle extends particleSystemBase {
 
     /**
      * @description: 获取当前粒子属性
-     * @return {fireStyleType}
+     * @return {fireInitialType}
      */
     get style(){
         return this.#style;
@@ -64,25 +67,14 @@ export default class fireParticle extends particleSystemBase {
 
     /**
      * @description: 更新样式
-     * @param {fireStyleType} style 要更新的样式
+     * @param {fireUpdateType} style 要更新的样式
      * @return {*}
      */
-    set style(style: fireStyleType){
-        for (let i in style) {
-            if (i === "imageSize") {
-                // @ts-ignore
-                this.particleSystem.imageSize = style.particleSize
-                    ? new Cesium.Cartesian2(
-                            style.particleSize,
-                            style.particleSize
-                    ) : new Cesium.Cartesian2(
-                            this.#style.particleSize,
-                            this.#style.particleSize
-                    );
-            } else {
-                // @ts-ignore
-                this.particleSystem[i] = style[i];
-            }
+    set style(style: fireUpdateType){
+        let i: keyof fireUpdateType;
+        for (i in style) {
+            this.particleSystem[i] = style[i]!;
+            this.#style[i] = style[i]
         }
     }
 
@@ -116,9 +108,9 @@ export default class fireParticle extends particleSystemBase {
 
     /**
      * @description: 默认样式
-     * @return {fireStyleType}
+     * @return {fireInitialType}
      */
-    get defaultStyle(): fireStyleType {
+    get defaultStyle(): fireInitialType {
         return {
             image: require("../../assets/img/fireParticle/fire.png"),
             startScale: 3,
