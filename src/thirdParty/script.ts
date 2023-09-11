ac4b5d0ea8660b58bd2a68c2c1f3bd19aee9c240
XingTao <xingt@geovis.com.cn>
2023-09-11 11:02:25 +0800

    添加turf

--- src/thirdParty/script.ts
+++ src/thirdParty/script.ts
@@ -1,10 +1,12 @@
 import * as Cesium from "cesium";
 import * as mapv from "@/thirdParty/mapv/mapv.js";
+import * as turf from "@turf/turf";
 import MapVLayer from "@/thirdParty/mapv/MapVLayer.js";
 
 window.Cesium = Cesium;
 window.mapv = mapv;
 window.MapVLayer = MapVLayer;
+window.turf = turf;
 // token
 const cesiumToken =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmOTNiYjkwZi1iMzRlLTRjZWQtYWQxMy00MDVmMjk4YTc0YmMiLCJpZCI6MzY3MDksImlhdCI6MTY1NTE3OTc1N30.fv4nNIkCEEy3VqlaekWVcE1btEcge5_zCl_36AtusT0";
