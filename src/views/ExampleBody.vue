e4d4844226660267ec2ac1bd801a9f6f66aea6d5
XingTao <xingt@geovis.com.cn>
2023-09-14 19:38:42 +0800

    添加拾取坐标组件

--- src/views/ExampleBody.vue
+++ src/views/ExampleBody.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="example-body">
         <BaseEarth></BaseEarth>
+        <PositionBox></PositionBox>
         <router-view />
     </div>
 </template>
 
 <script setup lang="ts">
 import BaseEarth from "@/components/earth/BaseEarth.vue";
+import PositionBox from "@/components/earth/PositionBox.vue";
 </script>
 
 <style lang="scss" scoped>
