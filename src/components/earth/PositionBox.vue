583eecf11ef6f209929151eb03cfc5bef1c61593
XingTao <xingt@geovis.com.cn>
2023-09-14 19:38:25 +0800

    拾取坐标组件

---
+++ src/components/earth/PositionBox.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="position-box">
+        <span class="position-box-label">经度：{{ lon }}</span>
+        <span class="position-box-label">纬度：{{ lat }}</span>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import { Ref, onBeforeUnmount, onMounted, ref } from "vue";
+import { ScreenSpaceEventHandler } from "cesium";
+import cartographicTool from "@/secdev/utils/cartographicTool";
+
+const lon: Ref<"未知"|number> = ref("未知");
+const lat: Ref<"未知"|number> = ref("未知");
+
+let handler: ScreenSpaceEventHandler;
+onMounted(()=>{
+    handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+    handler.setInputAction((movement)=>{
+        let cartesian = viewer.camera.pickEllipsoid(movement.endPosition);
+        if (cartesian) {
+            let degrees = cartographicTool.formCartesian3(cartesian);
+            lon.value = degrees.longitude;
+            lat.value = degrees.latitude;
+        } else {
+            lon.value = "未知";
+            lat.value = "未知";
+        }
+    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE)
+})
+
+onBeforeUnmount(()=>{
+    handler && handler.removeInputAction(Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+    handler && handler.destroy();
+})
+
+</script>
+
+<style lang='scss' scoped>
+@import "./assets/style/PositionBox.scss"
+
+</style>
