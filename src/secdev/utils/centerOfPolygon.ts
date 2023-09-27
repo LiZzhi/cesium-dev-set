6da22feea0426094c04f68a58f1258d4679e8c4f
XingTao <xingt@geovis.com.cn>
2023-09-27 20:16:21 +0800

    计算多边形重心

---
+++ src/secdev/utils/centerOfPolygon.ts
@@ -0,0 +1,15 @@
+import { Cartesian3 } from "cesium";
+import cartographicTool from "./cartographicTool";
+
+export default function centerOfPolygon(positions: Cartesian3[]){
+    let cats = cartographicTool.formCartesian3S(positions);
+    let coordinates = cats.map(cat => [cat.longitude, cat.latitude]);
+    let start = coordinates[0];
+    let end = coordinates[coordinates.length-1];
+    if(start[0] !== end[0] || start[1] !== end[1]){
+        coordinates.push(start);
+    }
+    let polygon = turf.polygon([coordinates]);
+    let coord = turf.centerOfMass(polygon).geometry.coordinates;
+    return Cesium.Cartesian3.fromDegrees(coord[0], coord[1])
+}
