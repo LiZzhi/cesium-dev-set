4b00b5f4cda5dab304c2b4190f057a665feb05f4
“Lizhi” <362042734@qq.com>
2023-10-06 22:07:40 +0800

    路径规划

--- src/components/example/spatialAnalysis/PathPlanning.vue
+++ src/components/example/spatialAnalysis/PathPlanning.vue
@@ -49,21 +49,69 @@
                 </div>
             </div>
             <div class="plan-btn-box">
-                <CommButton @click="" class="plan">规划</CommButton>
-                <CommButton @click="" class="clear">清除</CommButton>
+                <CommButton @click="requset" class="plan">规划</CommButton>
+                <CommButton @click="closePanel" class="clear">清除</CommButton>
+            </div>
+        </div>
+    </CommPanel>
+    <CommPanel
+        title="规划结果"
+        class="result-panel-box"
+        v-if="show"
+        @close="closePanel"
+    >
+        <div class="result-item-container">
+            <div
+                v-for="(item, i) in result"
+                :key="i"
+                class="result-item-box"
+            >
+                <div class="result-item-content">
+                    <span class="result-item-title">路程全长（米）：</span>
+                    <span class="result-item-body">{{ item.distance }}</span>
+                </div>
+                <div class="result-item-content">
+                    <span class="result-item-title">路程耗时（秒）：</span>
+                    <span class="result-item-body">{{ item.duration }}</span>
+                </div>
+                <div class="result-item-content">
+                    <span class="result-item-title">红绿灯数（个）：</span>
+                    <span class="result-item-body">{{ item.lights }}</span>
+                </div>
+                <div class="result-item-content">
+                    <span class="result-item-title">收费路段（米）：</span>
+                    <span class="result-item-body">{{ item.toll }}</span>
+                </div>
+                <div class="result-item-content">
+                    <span class="result-item-title">路程说明：</span>
+                    <span class="result-item-body">{{ item.instruction }}</span>
+                </div>
+                <div class="result-item-content">
+                    <span class="result-item-title" style="align-self: center;">显示路线：</span>
+                    <span class="result-item-body">
+                        <CommButton @click="showLine(item.entity)">显示</CommButton>
+                    </span>
+                </div>
             </div>
         </div>
     </CommPanel>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref, Ref } from "vue";
+import { computed, onMounted, ref, Ref, watch } from "vue";
 import drawShape from "@/secdev/specialEffectPlot/plot/drawShape";
+import polylineLinkPulseMaterial from "@/secdev/specialEffectPlot/lineMaterial/polylineLinkPulseMaterial.js";
 import cartographicTool from "@/secdev/utils/cartographicTool";
+import coordinateOffset from "@/secdev/utils/coordinateOffset";
 import entityFactory from "@/secdev/utils/entityFactory";
 import { Entity } from "cesium";
+import server from "@/server";
+import api from "@/server/aMap/api";
+import { ElMessage } from "element-plus";
 
 let draw: drawShape;
+const http = server.aMapServer;
+const key = "432b84bd33de91d47d19a6cef894c723";
 
 onMounted(() => {
     draw = new drawShape(viewer);
@@ -156,6 +204,143 @@ const clearAvoidancePoint = ()=>{
         viewer.entities.remove(e);
     })
     avoidanceEntities.length = 0;
+}
+
+type resultType = {
+    entity: Entity;
+    distance: string;
+    duration:string;
+    lights: string;
+    toll: string;
+    instruction: string;
+}
+let show = ref(false);
+let result: Ref<resultType[]> = ref([]);
+let lineEntity: undefined|Entity = undefined;
+const requset = async () => {
+    if(!(startPoint.value && endPoint.value)){
+        ElMessage.warning("起点和终点为必选项");
+        return;
+    }
+    // 途径点
+    let approach = "";
+    if(approachPoint.value.length){
+        for (let i = 0; i < approachPoint.value.length; i++) {
+            if(i !== 0){
+                approach += ";";
+            }
+            let cur = approachPoint.value[i];
+            // 84转高德(火星)
+            cur = coordinateOffset.gcj_encrypt(cur[0], cur[1]);
+            approach += `${cur[0].toFixed(6)},${cur[1].toFixed(6)}`;
+        }
+    }
+    // 避让区
+    let avoidance = "";
+    if(avoidanceArea.value.length){
+        for (let i = 0; i < avoidanceArea.value.length; i++) {
+            if(i !== 0){
+                avoidance += "|";
+            }
+            let areaStr = "";
+            const curArea = avoidanceArea.value[i];
+            for (let j = 0; j < curArea.length; j++) {
+                if(j !== 0){
+                    areaStr += ";";
+                }
+                let cur = curArea[i];
+                // 84转高德(火星)
+                cur = coordinateOffset.gcj_encrypt(cur[0], cur[1]);
+                areaStr += `${cur[0].toFixed(6)},${cur[1].toFixed(6)}`;
+            }
+            avoidance += `${areaStr}`
+        }
+    }
+
+    result.value.length = 0;
+    closePanel();
+
+    // wgs84转火星
+    let originArr = startPoint.value.split(",");
+    let origin = coordinateOffset.gcj_encrypt(Number(originArr[0]), Number(originArr[1])).join(",");
+
+    let destinationArr = endPoint.value.split(",");
+    let destination = coordinateOffset.gcj_encrypt(Number(destinationArr[0]), Number(destinationArr[1])).join(",");
+    const res = await http.get(api.driving, {
+        params: {
+            key: key,
+            origin: origin,
+            destination: destination,
+            strategy: "10", // 策略，当前为高德地图默认策略
+            waypoints: approach,
+            avoidpolygons: avoidance,
+            output: "JSON", // 返回数据格式类型
+            extensions: "all",  // 返回全部信息
+        }
+    })
+    if(res.status === 200 && res.data.status === "1"){
+        let data = res.data.route.paths;
+        for (let i = 0; i < data.length; i++) {
+            const element = data[i];
+            const steps = element.steps;
+            let instruction = "";
+            let line = "";
+            for (let j = 0; j < steps.length; j++) {
+                if(j !== 0){
+                    instruction += ",";
+                    line += ";";
+                }
+                instruction += steps[j].instruction;
+                line += steps[j].polyline;
+            }
+            result.value.push({
+                distance: element.distance + "米",
+                duration: element.duration + "秒",
+                lights: element.traffic_lights + "个",
+                toll: element.toll_distance + "米",
+                instruction: instruction + "。",
+                entity: new Cesium.Entity({
+                    id: '路径规划-方案-' + i,
+                    name: '路径规划-方案-' + i,
+                    polyline: {
+                        positions: analyticCoord(line),
+                        width: 15,
+                        material: new polylineLinkPulseMaterial({
+                            color: Cesium.Color.fromRandom().withAlpha(0.7),
+                            duration: 5000 //时间 控制速度
+                        }),
+                        clampToGround: true
+                    }
+                })
+            })
+        }
+        show.value = true;
+    }
+}
+
+const showLine = (e: Entity)=>{
+    lineEntity && viewer.entities.remove(lineEntity);
+    lineEntity = e;
+    viewer.entities.add(e);
+}
+
+const closePanel = ()=>{
+    show.value = false;
+    lineEntity && viewer.entities.remove(lineEntity);
+    lineEntity = undefined;
+}
+
+const analyticCoord = (coordStr:string) => {
+    let result: number[] = [];
+    let coords = coordStr.split(";");
+    coords.forEach((v)=>{
+        let c = v.split(",");
+        // 高德(火星)转84
+        let coord84 = coordinateOffset.gcj_decrypt(Number(c[0]), Number(c[1]));
+        result.push(coord84[0]);
+        result.push(coord84[1]);
+    })
+    return Cesium.Cartesian3.fromDegreesArray(result);
 }
 </script>
 
